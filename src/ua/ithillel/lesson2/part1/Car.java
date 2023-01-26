a1e720618f3b7132d0ab32b5bfae47df634f646d
BlondGT <112488079+BlondGT@users.noreply.github.com>
2023-01-26 13:31:35 +0200

    Changed methods in package Car

--- src/ua/ithillel/lesson2/part1/Car.java
+++ src/ua/ithillel/lesson2/part1/Car.java
@@ -2,17 +2,17 @@ package ua.ithillel.lesson2.part1;
 
 public class Car {
 
-    public void startElectricity () {
+    void startElectricity() {
 
         System.out.println("Electricity system");
     }
 
-    public void startCommand () {
+    void startCommand() {
 
         System.out.println("Command system");
     }
 
-    public void startFuelSystem () {
+    void startFuelSystem() {
 
         System.out.println("Fuel system");
     }
